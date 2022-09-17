3fe7270b2f51e4576f1e658115d94af6044528db
kkoltong01 <kkoltong015@gmail.com>
2022-09-17 21:51:08 +0900

    spring02

---
+++ board2/src/main/java/com/board/entity/Board.java
@@ -0,0 +1,13 @@
+package com.board.entity;
+
+import lombok.Data;
+
+@Data // lombok API
+public class Board {
+	private int idx;
+	private String title;
+	private String content;
+	private String writer;
+	private String indate;
+	private int count;
+}
